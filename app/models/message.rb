6309ebbdf959ea6f49ec81b27083e34a3b46c54f
QianYanDai <qianyan.dai@striking.ly>
2017-11-23 15:03:08 +0800

    Only user mode

--- app/models/message.rb
+++
@@ -1,7 +0,0 @@
-class Message < ApplicationRecord
-  # belongs_to :user
-  # validates :user_id, presence: true
-  # default_scope -> { order(created_at: :desc) }
-  after_create_commit { MessageBroadcastJob.perform_later self }
-  
-end
